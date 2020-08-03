26eec2d5b09e0d478d0d025cf92f9ed20119a6dd
Nauman Munir <naumanmunir@gmail.com>
2020-08-03 10:25:47 +0500

    local storage

--- app.js
+++ app.js
@@ -6,9 +6,20 @@ const movieSelect = document.getElementById("movie");
 
 let ticketPrice = +movieSelect.value;
 
+// save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
 // update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
   const selectedSeatsCounts = selectedSeats.length;
   count.innerText = selectedSeatsCounts;
   total.innerText = selectedSeatsCounts * ticketPrice;
@@ -17,6 +28,7 @@ function updateSelectedCount() {
 // movie select event
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
