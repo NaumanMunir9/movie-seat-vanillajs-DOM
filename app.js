5d1de03ddd9b3dd82776cbf6cd747dfe650d1293
Nauman Munir <naumanmunir@gmail.com>
2020-08-20 22:24:36 +0500

    setMovieData

--- app.js
+++ app.js
@@ -6,9 +6,21 @@ const movieSelect = document.getElementById("movie");
 
 let ticketPrice = +movieSelect.value;
 
+// save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedM<oviePrice", moviePrice);
+}
+
 // Update total and count
 function updateSelectedCount() {
   const seletedSeats = document.querySelectorAll(".row .seat.selected");
+
+  // copy selected seats into array => Map through the Array => return a new array indexes
+  const seatsIndex = [...seletedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = seletedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
@@ -17,6 +29,7 @@ function updateSelectedCount() {
 // movie select event listener
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
